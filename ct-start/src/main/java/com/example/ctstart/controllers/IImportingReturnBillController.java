9ac2ee2568b1f01ba02414893b1b7ef50df0b846
NQTien0702 <149856189+NQTien0702@users.noreply.github.com>
2024-03-04 11:11:56 +0700

    commit

--- ct-start/src/main/java/com/example/ctstart/controllers/IImportingReturnBillController.java
+++ ct-start/src/main/java/com/example/ctstart/controllers/IImportingReturnBillController.java
@@ -1,5 +1,6 @@
 package com.example.ctstart.controllers;
 
+import com.example.ctapi.dtos.response.ExportingBillFullDto;
 import com.example.ctapi.dtos.response.ImportingReturnBillFullDto;
 import com.example.ctapi.dtos.response.ResponseDto;
 import com.example.ctapi.serviceImpl.IExportingServiceImpl;
@@ -25,6 +26,7 @@ public class IImportingReturnBillController {
     private final SimpMessagingTemplate messagingTemplate;
     @Autowired
     final IImportingReturnService iImportingReturnService;
+
     @PostMapping("/create")
     public ResponseEntity<?> createReturnBill(@RequestBody ImportingReturnBillFullDto importingReturnBIll) {
         try {
@@ -37,15 +39,64 @@ public class IImportingReturnBillController {
                     HttpStatus.INTERNAL_SERVER_ERROR.value(), null));
         }
     }
-//    @GetMapping("/{id}")
-//    public ResponseEntity<?> getImportingReturnById(HttpServletRequest request, @PathVariable String id) {
-//        try {
-//            var result = iImportingReturnService.getExportingById(request, id);
-//            return ResponseEntity.ok(new ResponseDto(List.of("data get sucess"),
-//                    HttpStatus.OK.value(), result));
-//        } catch (RuntimeException | IOException e) {
-//            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error get Payment: " + e.getMessage());
-//        }
-//    }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getImportingReturnById(HttpServletRequest request, @PathVariable String id) {
+        try {
+            var result = iImportingReturnService.getImportingReturnById(request, id);
+            return ResponseEntity.ok(new ResponseDto(List.of("data get sucess"),
+                    HttpStatus.OK.value(), result));
+        } catch (RuntimeException | IOException e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error get Payment: " + e.getMessage());
+        }
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<?> deteleImportingreturn(@PathVariable String id) {
+        try {
+            iImportingReturnService.deleteImportingReturnfullByid(id);
+            return ResponseEntity.ok(new ResponseDto(List.of("get all importing success"),
+                    HttpStatus.OK.value(), "deleted : " + id));
+        } catch (Exception e) {
+            logger.error(e.getMessage(), e);
+            return ResponseEntity.ok(new ResponseDto(List.of("get all importing unsuccess"),
+                    HttpStatus.INTERNAL_SERVER_ERROR.value(), null));
+        }
+    }
+    @PostMapping ("/getAllExportingBill")
+    public ResponseEntity<?> getAllExportingbill(HttpServletRequest request) {
+        try {
+            List<ImportingReturnBillFullDto> result = iImportingReturnService.getAllImportingbReturnill(request);
+            return ResponseEntity.ok(new ResponseDto(List.of("get all bill success"),
+                    HttpStatus.OK.value(), result));
+        } catch (Exception e) {
+            logger.error(e.getMessage(), e);
+            return ResponseEntity.ok(new ResponseDto(List.of("get all bill unsuccess"),
+                    HttpStatus.INTERNAL_SERVER_ERROR.value(), null));
+        }
+    }
+    @PostMapping ("/findimportingReturnAll")
+    private ResponseEntity<?> seachAllImportingReturn(HttpServletRequest request) {
+        try {
+            int a=0;
+            var result = iImportingReturnService.getAllImportingReturnBillUseBaseSearch(request);
+            return ResponseEntity.ok(new ResponseDto(List.of("Successful for find!"), HttpStatus.OK.value(), result));
+
+        } catch (RuntimeException | IOException e) {
+            logger.error(e.getMessage(), e);
+            return ResponseEntity.ok(new ResponseDto(List.of("ngu"+e.getMessage()), HttpStatus.INTERNAL_SERVER_ERROR.value(), null));
+        }
+    }
+
+    @PostMapping("/updateExporting")
+    public ResponseEntity<?> updateExporting(@RequestBody ImportingReturnBillFullDto ImportingReturnBillFull) {
+        try {
+            int a = 0;
+            iImportingReturnService.updateImportingReturn(ImportingReturnBillFull);
+            return ResponseEntity.ok(new ResponseDto(List.of("data Updatating success"),
+                    HttpStatus.OK.value(), ImportingReturnBillFull));
+        } catch (RuntimeException e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error updating Payment: " + e.getMessage());
+        }
+    }
 }
